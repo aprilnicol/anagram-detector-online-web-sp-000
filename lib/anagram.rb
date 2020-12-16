81dee8dd5c4a4f259b582f30388f60034179439a
April Adams <illusionlace@gmail.com>
2020-12-16 11:28:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,8 @@ class Anagram
   attr_accessor :word
   def initialize(@word)
     @word = word
-  end  
+  end
+end  
 def match(words)
   word_array.select do |word|
     word.split("").sort == @word.split("").sort
